369f62ffc55378e4fe7b30cedeaed8318cff0a43
JIUUOO <nninjiuuoo@gmail.com>
2024-11-27 16:33:47 +0900

    ✨ Feat: 로티파일즈 애니메이션 spalsh screen으로 추가, eas-cli 재설치, eas 재구성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,22 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View } from "react-native";
+import LottieView from "lottie-react-native";
 import WebView from "react-native-webview";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  const [isLoading, setIsLoading] = useState(true); // 로티 애니메이션 및 웹뷰 로딩 상태 관리
 
   useEffect(() => {
     if (loaded) {
@@ -35,11 +30,22 @@ export default function RootLayout() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar style="auto" />
-      <WebView
-        style={{ flex: 1 }}
-        source={{ uri: "https://youthmap.site/home" }}
-      />
+      {isLoading ? (
+        <View style={{ flex: 1, width: "100%", height: "100%" }}>
+          <LottieView
+            source={require("../assets/animations/splash-screen.json")}
+            style={{ width: "100%", height: "100%" }}
+            autoPlay
+            loop={false}
+            onAnimationFinish={() => setIsLoading(false)}
+          />
+        </View>
+      ) : (
+        <WebView
+          style={{ flex: 1 }}
+          source={{ uri: "https://youthmap.site/home" }}
+        />
+      )}
     </SafeAreaView>
   );
 }
